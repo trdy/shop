5dfc5e7529c3d8778e2333683f9bf1a6f3125f66
trdy <liyun_jz@126.com>
2017-11-27 14:26:53 +0800

    2017.11.27

--- src/main/java/com/yt/shop/common/interceptor/TestInterceptor.java
+++ src/main/java/com/yt/shop/common/interceptor/TestInterceptor.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class TestInterceptor implements HandlerInterceptor {
 
     private Logger log= LoggerFactory.getLogger(this.getClass());
@@ -15,12 +16,13 @@ public class TestInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         log.info("call TestInterceptor preHeadle method .....");
-        return false;
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
         log.info("call TestInterceptor postHandle method .....");
+
     }
 
     @Override

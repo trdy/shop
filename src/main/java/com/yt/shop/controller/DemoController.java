b2333e470e1ece3db70c6fe762d6decbe4e868d2
trdy <liyun_jz@126.com>
2017-11-29 09:53:20 +0800

    2017.11.29

--- src/main/java/com/yt/shop/controller/DemoController.java
+++ src/main/java/com/yt/shop/controller/DemoController.java
@@ -1,18 +1,22 @@
 package com.yt.shop.controller;
 
+import com.yt.shop.common.FileUtil;
 import com.yt.shop.model.Person;
 import com.yt.shop.service.DemoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-@RestController
+@Controller
 public class DemoController {
 
     private Logger log= LoggerFactory.getLogger(this.getClass());
@@ -22,6 +26,7 @@ public class DemoController {
 
 
     @RequestMapping("/person/savePerson")
+    @ResponseBody
     public int savePerson(@RequestBody Person person){
         log.info("测试保存person的方法");
         try {
@@ -36,6 +41,7 @@ public class DemoController {
     }
 
     @RequestMapping("/person/listPerson")
+    @ResponseBody
     public List<Person> listPerson(){
         log.info("测试查询person集合的方法");
         try {
@@ -45,6 +51,52 @@ public class DemoController {
         }catch (Exception e){
             log.error("查询person集合出错:"+e.getMessage());
             return new ArrayList<Person>();
+        }
+
+    }
+
+
+    @RequestMapping("/person/personUpload")
+    public String personUpload(){
+        return "personUpload";
+    }
+
+    @RequestMapping("/person/savePerson1")
+    @ResponseBody
+    public int savePerson1(@RequestParam("file") MultipartFile file, HttpServletRequest request){
+        log.info("call 文件上传");
+        int result=0;
+        try {
+            Person p = new Person();
+            String pname = request.getParameter("pname");
+            p.setPname(pname);
+            p.setBirth(new Timestamp(System.currentTimeMillis()));
+
+            if(!file.isEmpty()) {
+                String filePath = System.getProperty("user.dir") + "/upload/userHead/";
+                int n = file.getOriginalFilename().indexOf(".");
+                String fileName = UUID.randomUUID() + file.getOriginalFilename().substring(n);
+                log.info("上传文件目录：" + filePath);
+
+                try {
+                    FileUtil.uploadFile(file.getBytes(), filePath, fileName);
+                    p.setPimg("upload/userHead/" + fileName);
+                    log.info("上传成功");
+                    result=1;
+                } catch (Exception e) {
+                    log.error("执行文件上传：" + e.toString());
+
+                }
+            }else {
+                p.setPimg("/upload/userHead/defaultHead.jpg");
+            }
+
+            demoService.insertPerson(p);
+            log.debug("文件上传成功,数据正确保存："+p);
+            return result;
+        }catch (Exception e){
+            log.error("文件上传错误"+e.getMessage());
+            return result;
         }
 
     }
